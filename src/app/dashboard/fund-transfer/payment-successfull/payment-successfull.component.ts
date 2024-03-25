83ac712e9063e5f51f15573ea56cbd02cd018578
Sublari Leela Deepak <leeladeepak123@gmail.com>
2024-03-25 12:03:38 +0530

    payment changes

---
+++ src/app/dashboard/fund-transfer/payment-successfull/payment-successfull.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-successfull',
+  templateUrl: './payment-successfull.component.html',
+  styleUrl: './payment-successfull.component.css'
+})
+export class PaymentSuccessfullComponent {
+
+}
