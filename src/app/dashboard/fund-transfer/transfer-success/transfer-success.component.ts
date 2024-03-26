713cb66a5918c5e5f3db383b448520d129420775
KatyayaniDeviGatti <gattikatyayanidevi@gmail.com>
2024-03-26 17:16:31 +0530

    commit transfer success

---
+++ src/app/dashboard/fund-transfer/transfer-success/transfer-success.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transfer-success',
+  templateUrl: './transfer-success.component.html',
+  styleUrl: './transfer-success.component.css'
+})
+export class TransferSuccessComponent {
+
+}
